188dad3d137fa325a4c757c68f53934840a1f74f
Fish <vincentvenhuizen1997@gmail.com>
2024-08-30 00:02:45 +0200

    Quick fix for auto whitelist Set auto whitelist to false by default

--- src/main/java/com/misterfish/config/Config.java
+++ src/main/java/com/misterfish/config/Config.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Config extends MidnightConfig {
     @Entry(category = "text") public static boolean opRequired = false;
     @Entry(category = "text") public static boolean autoOp = true;
-    @Entry(category = "text") public static boolean autoWhitelist = true;
+    @Entry(category = "text") public static boolean autoWhitelist = false;
     @Entry(category = "text") public static boolean autoDisconnect = true;
     @Entry(category = "text") public static boolean killOnDeath = true;
     @Entry(category = "text") public static boolean respawnKickedPlayers = true;
